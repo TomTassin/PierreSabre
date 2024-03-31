e3c6c0f2955a592eecb62c8f8b606bbdf3fc73a1
TomTassin <ttassin150@gmail.com>
2024-03-31 21:10:45 +0200

    creation classe commercant 2.1)

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,15 +1,14 @@
 package histoire;
 
-import personnages.Humain;
+import personnages.Commercant;
 
 public interface HistoireTP4 {
 	
 	public static void main(String[] args) {
-		Humain prof = new Humain("Prof","kombucha",54);
-		prof.direBonjour();
-		prof.acheter("une boisson", 12);
-		prof.boire();
-		prof.acheter("un jeu", 2);
-		prof.acheter("kimono", 50);
+		Commercant marco = new Commercant("Marco","thé",54);
+		marco.direBonjour();
+		marco.seFaireExtorquer();
+		marco.recevoir(15);
+		marco.boire();
 	}
 }
