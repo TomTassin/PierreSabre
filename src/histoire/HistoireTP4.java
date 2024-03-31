5e3c0bebf1d94e6dd017e1bb7efc9a1c0e385b23
TomTassin <ttassin150@gmail.com>
2024-03-31 21:50:42 +0200

    creation classe ronins 2.3)

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,14 +1,15 @@
 package histoire;
 
 import personnages.Commercant;
+import personnages.Ronins;
 import personnages.Yakuzas;
 
 public interface HistoireTP4 {
 	
 	public static void main(String[] args) {
 		Commercant marco = new Commercant("Marco","thé",15);
-		Yakuzas yakuLeNoir = new Yakuzas("Yaku Le Noir", "whisky", 30, "Warsong");
-		yakuLeNoir.direBonjour();
-		yakuLeNoir.extorquer(marco);
+		Ronins roro = new Ronins("Roro","shochu",60);
+		roro.direBonjour();
+		roro.donner(marco);
 	}
 }
--- src/personnages/Ronins.java
+++ src/personnages/Ronins.java
@@ -1,9 +1,16 @@
 package personnages;
 
 public class Ronins extends Humain{
-
+	private int honneur=1;
+	
 	public Ronins(String nom, String boissonFavorite, int quantiteArgent) {
 		super(nom, boissonFavorite, quantiteArgent);
 	}
-
+	public void donner(Commercant beneficiaire) {
+		assert getQuantiteArgent()>10;
+		int don=(int) (getQuantiteArgent()*(0.1));
+		perdreArgent(don);
+		parler(beneficiaire.getNom() +" prend ces "+ don +" sous.");
+		beneficiaire.recevoir(don);
+	}
 }
